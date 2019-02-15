742c4c38c73997bf786dd5e82548dc30e7277f99
debarge <43295961+aodem@users.noreply.github.com>
2019-02-14 20:07:37 -0700

    push.3.2

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,4 @@
+
 const friends = require('../data/friends');
 
 module.exports = (app) => {
@@ -5,14 +6,40 @@ module.exports = (app) => {
         res.json(friends);
     })
     app.post("/api/friends", (req, res) => {
+        let bestMatch;
+        let score = 1000;
 
-        for(let i = 0; i < friends.length ; i++){
-            let unitDifference = 0;
-            let arrDifference = 0;
-            parseInt(friends[i].scores[i]) - req.body.scores[i]
+        let finalArr = [];
+        let count = 0;
+        matchMaker(req.body.scores, friends[0])
+
+
+        function matchMaker (newUserArray, currentUsers){
+            const exitComparison = friends.length;
+
+            for (let i = 0; i < 10; i++){
+                let difference = parseInt(newUserArray[i]) - parseInt(currentUsers.scores[i])
+
+                if(difference < 0){difference *= -1}
+                finalArr.push(difference)
+            }
+        
+            const sumNum = finalArr.reduce((accumulator, currentValue) => accumulator + currentValue)
+
+            if(sumNum < score){
+                score = sumNum
+                bestMatch = currentUsers
+            }
+            count += 1
+
+            console.log(exitComparison, finalArr, sumNum, bestMatch, count, score)
+
+            if(count < exitComparison){
+               matchMaker(newUserArray, friends[count]) 
+            } 
         }
 
         friends.push(req.body);
-        res.json(true);
+        res.json(bestMatch);
     })
 }
